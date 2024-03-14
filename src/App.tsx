0feedd9bb151645be1b33177a506462fc9bcaebe
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-14 17:02:19 -0300

    feat: add post's list

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,37 @@ import style from "./styles/app.module.css";
 
 import "./styles/global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/carlos-hfc.png",
+      name: "Carlos Faustino",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-03-14 13:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/carlos-hfc.png",
+      name: "Carlos Faustino",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-03-14 13:00:00'),
+  }
+];
+
 export function App() {
   return (
     <div>
@@ -15,9 +46,12 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
-          <Post />
+          {posts.map(item => (
+            <Post
+              key={item.id}
+              {...item}
+            />
+          ))}
         </main>
       </div>
     </div>
